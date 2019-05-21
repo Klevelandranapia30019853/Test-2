ec003528190b7b368002421c9ff5c3a780e7135d
Klevelandranapia30019853 <48032182+Klevelandranapia30019853@users.noreply.github.com>
2019-05-22 11:08:38 +1200

    Add files via upload

---
+++ Test-2/Classes/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+
+            Console.Write("Please enter the person's first name: ");
+            p1.Fname = Console.ReadLine();
+            Console.Write("Please enter the person's last name: ");
+            p1.Lname = Console.ReadLine();
+            Console.Write("Please enter the person's age: ");
+            p1.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nThank you, Here is the information you entered .....");
+            Console.ReadLine();
+            Console.WriteLine("Name: {0} {1}\nAge: {2}", p1.Fname, p1.Lname, p1.Age);
+            Console.ReadLine();
+        }
+    }
+
+    internal class Person
+    {
+        internal string Fname;
+        internal string Lname;
+        internal int Age;
+    }
+}
---
+++ Test-2/Classes/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salary s1 = new Salary();
+            Person p1 = new Person();
+
+            Console.Write("Please enter the person's first name: ");
+            p1.Fname = Console.ReadLine();
+            Console.Write("Please enter the person's last name: ");
+            p1.Lname = Console.ReadLine();
+
+            Console.WriteLine("This is the amount of salary you are getting per year");
+            Console.WriteLine("80,000");
+            Console.ReadLine();
+
+            Console.WriteLine("This is the amount you get paid per Week");
+            Console.WriteLine("$1536.46");
+            Console.ReadLine();
+        }
+    }
+
+    internal class Person
+    {
+        internal string Fname;
+        internal string Lname;
+    }
+
+    internal class Salary
+    {
+        internal string Fname;
+        internal string Lname;
+        internal int Wages;
+
+    }
+}
